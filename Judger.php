7444a4e5d6d74e62cc18d609fd3eaebaba596cbb
zsgsdesign <zsgsdesign@gmail.com>
2021-10-24 02:05:12 +0800

    complete Judger

--- Judger.php
+++ Judger.php
@@ -1,9 +1,10 @@
 <?php
-namespace App\Babel\Extension\template;//The 'template' should be replaced by the real oj code.
+namespace App\Babel\Extension\gym;
 
 use App\Babel\Submit\Curl;
 use App\Models\Submission\SubmissionModel;
 use App\Models\JudgerModel;
+use App\Models\OJModel;
 use Requests;
 use Exception;
 use Log;
@@ -12,70 +13,112 @@ class Judger extends Curl
 {
 
     public $verdict=[
-        'Accepted'=>"Accepted",
-        "Presentation Error"=>"Presentation Error",
-        'Time Limit Exceeded'=>"Time Limit Exceed",
-        "Memory Limit Exceeded"=>"Memory Limit Exceed",
-        'Wrong Answer'=>"Wrong Answer",
-        'Runtime Error'=>"Runtime Error",
-        'Output Limit Exceeded'=>"Output Limit Exceeded",
-        'Compile Error'=>"Compile Error",
+        "COMPILATION_ERROR"=>"Compile Error",
+        "RUNTIME_ERROR"=> "Runtime Error",
+        "WRONG_ANSWER"=> "Wrong Answer",
+        "TIME_LIMIT_EXCEEDED"=>"Time Limit Exceed",
+        "OK"=>"Accepted",
+        "MEMORY_LIMIT_EXCEEDED"=>"Memory Limit Exceed",
+        "PRESENTATION_ERROR"=>"Presentation Error",
+        "IDLENESS_LIMIT_EXCEEDED"=>"Idleness Limit Exceed"
     ];
-    private $model=[];
-    private $template=[];
-
+    private $list = [];
+    private $csrf = [];
+    private $handles = [];
 
     public function __construct()
     {
-        $this->model["submissionModel"]=new SubmissionModel();
-        $this->model["judgerModel"]=new JudgerModel();
+        $this->submissionModel = new SubmissionModel();
+        $this->judgerModel = new JudgerModel();
+        $this->oid=OJModel::oid('codeforces');
+
+        if(is_null($this->oid)) {
+            throw new Exception("Online Judge Not Found");
+        }
     }
 
     public function judge($row)
     {
-        $sub=[];
-
-        if (!isset($this->poj[$row['remote_id']])) {
-            $judgerDetail=$this->model["judgerModel"]->detail($row['jid']);
-            $this->appendPOJStatus($judgerDetail['handle'], $row['remote_id']);
-            if (!isset($this->poj[$row['remote_id']])) {
-                return;
+        $cf=[];
+        if (!isset($this->handles[$row['jid']])) {
+            $handle = $this->judgerModel->detail($row['jid'])['handle'];
+            $this->handles[$row['jid']] = $handle;
+            $res=$this->grab_page([
+                'site' => 'https://codeforces.com',
+                'oj' => 'codeforces',
+                'handle' => $handle,
+            ]);
+            preg_match('/<meta name="X-Csrf-Token" content="([0-9a-z]*)"/', $res, $match);
+            $this->csrf[$row['jid']] = $match[1];
+            $this->list = array_merge($this->list, $this->get_last_codeforces($this->submissionModel->countEarliestWaitingSubmission($this->oid) + 5, $handle));
+        }
+        foreach ($this->list as $c) {
+            if ($c[3]==$row["remote_id"]) {
+                $cf=$c;
+                break;
             }
         }
+        if (empty($cf)) {
 
-        $status=$this->poj[$row['remote_id']];
-        $sub['verdict']=$this->verdict[$status['verdict']];
+            // $this->MODEL->updateSubmission($row['sid'], ['verdict'=>"Submission Error"]);
+        } else {
+            if (isset($this->verdict[$cf[2]])) {
+                $sub=[];
+                $sub['verdict']=$this->verdict[$cf[2]];
+                if ($sub['verdict']=='Compile Error') {
+                    $res=$this->post_data([
+                        'site' => 'https://codeforces.com/data/judgeProtocol',
+                        'data' => [
+                            'submissionId'=>$row['remote_id'],
+                            'csrf_token'=>$this->csrf[$row['jid']],
+                        ],
+                        'oj' => 'codeforces',
+                        'ret' => true,
+                        'returnHeader' => false,
+                        'handle' => $this->handles[$row['jid']],
+                    ]);
+                    $sub['compile_info']=json_decode($res);
+                }
+                $sub["score"]=$sub['verdict']=="Accepted" ? 1 : 0;
+                $sub['time']=$cf[0];
+                $sub['memory']=$cf[1];
+                $sub['remote_id']=$cf[3];
 
-        if ($sub['verdict']=='Compile Error') {
-            try {
-                $res=Requests::get('http://poj.org/showcompileinfo?solution_id='.$row['remote_id']);
-                preg_match('/<pre>([\s\S]*)<\/pre>/', $res->body, $match);
-                $sub['compile_info']=html_entity_decode($match[1], ENT_QUOTES);
-            } catch (Exception $e) {
+                // $ret[$row['sid']]=[
+                //     "verdict"=>$sub['verdict']
+                // ];
+
+                $this->submissionModel->updateSubmission($row['sid'], $sub);
             }
         }
-
-        $sub["score"]=$sub['verdict']=="Accepted" ? 1 : 0;
-        $sub['time']=$status['time'];
-        $sub['memory']=$status['memory'];
-        $sub['remote_id']=$row['remote_id'];
-
-        $this->model["submissionModel"]->updateSubmission($row['sid'], $sub);
     }
 
-    private function appendPOJStatus($judger, $first=null)
+    private function get_last_codeforces($num, $handle)
     {
-        if ($first!==null) {
-            $first++;
+        // \Log::debug('num:'.$num);
+        $ret=array();
+        if ($num==0) {
+            return $ret;
         }
-        $res=Requests::get("http://poj.org/status?user_id={$judger}&top={$first}");
-        $rows=preg_match_all('/<tr align=center><td>(\d+)<\/td><td>.*?<\/td><td>.*?<\/td><td>.*?<font color=.*?>(.*?)<\/font>.*?<\/td><td>(\d*)K?<\/td><td>(\d*)(?:MS)?<\/td>/', $res->body, $matches);
-        for ($i=0; $i<$rows; $i++) {
-            $this->poj[$matches[1][$i]]=[
-                'verdict'=>$matches[2][$i],
-                'memory'=>$matches[3][$i] ? $matches[3][$i] : 0,
-                'time'=>$matches[4][$i] ? $matches[4][$i] : 0,
-            ];
+
+        $ch=curl_init();
+        $url="https://codeforces.com/api/user.status?handle={$handle}&from=1&count={$num}";
+        curl_setopt($ch, CURLOPT_CAINFO, babel_path("Cookies/cacert.pem"));
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response=curl_exec($ch);
+        // \Log::debug('curl error:' . curl_error($ch));
+        curl_close($ch);
+        // \Log::debug('res:' . $response);
+        $result=json_decode($response, true);
+        if ($result["status"]=="OK") {
+            for ($i=0; $i<$num; $i++) {
+                if (!isset($result["result"][$i]["verdict"])) {
+                    return array_reverse($ret);
+                }
+                array_push($ret, array($result["result"][$i]["timeConsumedMillis"], $result["result"][$i]["memoryConsumedBytes"] / 1000, $result["result"][$i]["verdict"], $result["result"][$i]["id"]));
+            }
         }
+        return array_reverse($ret);
     }
 }
